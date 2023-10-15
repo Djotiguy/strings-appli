ad1d9fa46221bbadd657538b8a6aaabbbb2dc411
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-15 17:54:30 +0200

    load admin user. refactor db connection

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,30 +1,22 @@
-import { Client } from "pg";
-import {loadEnvConfig} from '@next/env';
 import {faker} from '@faker-js/faker';
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
+import bcrypt from 'bcrypt';
+import { getClient } from "@/db";
 
 async function loadFakeData(numUsers: number = 10){
     console.log(`${numUsers} users loaded!`);
 
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
-    });
+    const client = await getClient();
 
     await client.connect();
 
     try {
         await client.query('begin');
-        
         for(let i = 0; i < numUsers; i++){
+            const saltRounds = 10;
+            const hash = await bcrypt.hash("string123", saltRounds);  
             await client.query(
                 "insert into public.users (username, password, avatar) values ($1, $2, $3)",
-                [faker.internet.userName(), "password", faker.image.avatar()]
+                [faker.internet.userName(), hash, faker.image.avatar()]
             );
         }
 
