934f6bac015cbf32d09e2f2da677c49db3661179
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-18 14:53:42 +0200

    pages stub

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -4,18 +4,23 @@ export default function NavBar() {
   return (
     <nav className="flex max-w-md w-full p-5 bg-slate-800 rounded-lg">
       <ul className="flex flex-row justify-around w-full">
+
         <li>
           <Link href="/feed" className=""> Feed</Link>
         </li>
+
         <li>
-          <Link href="/profile" className=""> Profil </Link>
+          <Link href="/profile" className=""> Profile </Link>
         </li>
+
         <li>
           <Link href="/following" className=""> Following </Link>
         </li>
+
         <li>
-          <Link href="/follower" className=""> Follower </Link>
+          <Link href="/followers" className=""> Followers </Link>
         </li>
+
       </ul>
     </nav>
   )
