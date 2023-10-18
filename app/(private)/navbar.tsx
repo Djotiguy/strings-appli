48b95bb4b700238ae1d76d9bc9c5444c8d2bc68d
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-18 14:40:38 +0200

    style nav & footer

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
-    <nav>navbar</nav>
+    <nav className="flex max-w-md w-full p-5 bg-slate-800 rounded-lg">
+      <ul className="flex flex-row justify-around w-full">
+        <li>
+          <Link href="/feed" className=""> Feed</Link>
+        </li>
+        <li>
+          <Link href="/profile" className=""> Profil </Link>
+        </li>
+        <li>
+          <Link href="/following" className=""> Following </Link>
+        </li>
+        <li>
+          <Link href="/follower" className=""> Follower </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
