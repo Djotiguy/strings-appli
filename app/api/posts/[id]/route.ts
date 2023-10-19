9e5821339aa4578f25c782c6546bd2262f45000e
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-19 14:23:35 +0200

    update endpoint

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -13,3 +13,17 @@ export async function GET(request: Request,
     }
     return NextResponse.json({data: res.rows[0]});
 }
+
+
+export async function PATCH(request: Request, {params} : {params: {id: number}}){
+    const body = await request.json();
+    const jwtPayload = await getJWTPayLoad();
+    const res = await sql("select * from posts where user_id = $1 and id = $2",[jwtPayload.sub, params.id]);
+
+    if(res.rowCount == 0){
+        return NextResponse.json({error:"Not found"}, {status: 404});
+    }
+    
+    await sql("update posts set content = $1 where user_id = $2 and id = $3", [body.content, jwtPayload.sub, params.id]);
+    return NextResponse.json({message: "Post updated"});
+}
