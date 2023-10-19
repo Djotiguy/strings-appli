f976f4c3698120a01c0b2d6755190d003eb47355
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-19 14:33:35 +0200

    delete post endpoint

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -23,7 +23,20 @@ export async function PATCH(request: Request, {params} : {params: {id: number}})
     if(res.rowCount == 0){
         return NextResponse.json({error:"Not found"}, {status: 404});
     }
-    
+
     await sql("update posts set content = $1 where user_id = $2 and id = $3", [body.content, jwtPayload.sub, params.id]);
     return NextResponse.json({message: "Post updated"});
+}
+
+
+export async function DELETE(request: Request, {params} : {params: {id: number}}){
+    const jwtPayload = await getJWTPayLoad();
+    const res = await sql("delete from posts where user_id = $1 and id= $2", [jwtPayload.sub, params.id]);
+
+    if(res.rowCount == 1){
+        return NextResponse.json({message: "Post deleted"});
+    } else {
+        return NextResponse.json({error:"Not found"}, {status: 404});
+    }
+
 }
