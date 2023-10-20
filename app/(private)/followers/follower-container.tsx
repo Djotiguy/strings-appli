fd7d08bc5665bcba01476c6cb3adf13fbb210667
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-20 12:08:22 +0200

    followers page ui and api

---
+++ app/(private)/followers/follower-container.tsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import FollowersList from './follower-list';
+
+export default function FollowerContainer() {
+    const [cnt, setCnt] = useState(0);
+    const pages = []
+    for(let i = 0; i < cnt; i++){
+        pages.push(<FollowersList index={i} key={i}/>)
+    }
+  return (
+    <div>
+        {pages}
+        <div className='flex justify-center w-full'>
+            <button onClick={() => setCnt(cnt + 1)} className='bg-slate-900 p-2 rounded-lg'>Load More</button>
+        </div>
+    </div>
+  )
+}
--- app/(private)/followers/page.tsx
+++ app/(private)/followers/page.tsx
@@ -1,7 +1,11 @@
-export default async function Followers(){
+"use client";
+import FollowerContainer from "./follower-container";
+
+export default function Followers(){
     return(
         <main>
             <h2>Followers</h2>
+            <FollowerContainer />
         </main>
     )
 }
