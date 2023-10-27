ea35bc0bea7d55a2eda5b3cb7192f74e212865e7
Djotiguy <djotiguy@MacBook-Pro-de-Siaka.local>
2023-10-27 22:37:58 +0200

    finalize searchbar

--- app/(private)/search-bar.tsx
+++ app/(private)/search-bar.tsx
@@ -1,30 +1,67 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, useRef, useEffect, use } from "react";
 import * as _ from "lodash";
+import User from "../components/user";
 
 export default function SearchBar() {
-    const [searchResults, setSearchResults] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [visible, setVisible] = useState(true);
+  const debouncedSearch = _.debounce(fetcherSearchResults, 500);
+  const ref = useRef(null);
 
-    const debouncedSearch = _.debounce(fetcherSearchResults, 500);
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      // @ts-ignore
+      if(ref.current && !ref.current.contains(e.target)) {
+        setVisible(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
-    async function fetcherSearchResults(searchText: string){
-        const res = await fetch("/api/search?q=" +searchText);
-        if(res.ok){
-            const json = await res.json();
-            console.log(json);
-            
-            setSearchResults(json.data);
-        }
-
-
+  async function fetcherSearchResults(searchText: string) {
+    const res = await fetch("/api/search?q=" + searchText);
+    if (res.ok) {
+      const json = await res.json();
+      console.log(json);
+      setVisible(true);
+      setSearchResults(json.data);
+    } else {
+      setSearchResults([]);
+      setVisible(false);
     }
+  }
 
-    function  handleChange (e : ChangeEvent<HTMLInputElement>){
-        console.log("Change", e.target.value);
-        debouncedSearch(e.target.value);
-    }
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    console.log("Change", e.target.value);
+    debouncedSearch(e.target.value);
+  }
+  function hanleClick (e: React.MouseEvent<HTMLInputElement>) {
+    setVisible(true);
+  }
   return (
-    <div>
-        <input onChange={handleChange} type="text" className="p-2 rounded-lg bg-gray-700 my-2" placeholder="Search" />
+    <div className="flex flex-row max-w-md w-full justify-end relative">
+      <input
+        onChange={handleChange}
+        onClick={hanleClick}
+        type="text"
+        className="p-2 rounded-lg bg-gray-700 my-2 max-w-xs"
+        placeholder="Search"
+        ref={ref}
+      />
+     {visible && searchResults.length > 0 && (
+      <ul className="flex flex-col bg-gray-700 absolute p-2 rounded-lg top-14 w-full max-w-sm right-2">
+        {searchResults.map((res: UserI) => {
+          return (
+            <li key={res.id} className="my-3" onClick={() => setVisible(false)}>
+              <User user={res} />
+            </li>
+          );
+        })}
+      </ul>
+      )}
     </div>
-  )
+  );
 }
